e7bdf9738036af5c93afa6e5f5d71a1bccf2d03a
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2021-03-03 00:20:08 +0200

    Add test and base for the main scraping function
    - Main scraping function, scrape_booking_site(), calls two functions:
        - enter_search_parameters() --> Implemented in previous commit
        - scrape_through_search_results() --> Hardcoded return value;
            to be implemented next

--- application/scraping.py
+++ application/scraping.py
@@ -1,13 +1,49 @@
 import time
+from typing import Dict
+from typing import Optional
 
+from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from application.constants import CATEGORY_FIELD_XPATH
-from application.constants import LANGUAGE_FIELD_XPATH
+from application.constants import DRIVER_PATH
 from application.constants import ENGLISH_LANGUAGE_CHOICE_TEXT
+from application.constants import LANGUAGE_FIELD_XPATH
 from application.constants import OFFICE_FIELD_XPATH
 from application.constants import SEARCH_BUTTON_XPATH
 from application.constants import SERVICE_FIELD_XPATH
+from application.constants import WEBLINK
+
+
+def scrape_booking_site(category_choice_text: str,
+                        service_choice_text: str,
+                        office_choice_text: str) -> Optional[Dict[str, str]]:
+    """
+    Main scraping function that:
+        - creates a driver,
+        - goes to the booking site,
+        - enters the search parameters
+        - scrapes through the search results
+        - and returns the result dict {'available_time': available_time_str, 'date': date_str}
+    """
+    driver = webdriver.Chrome(DRIVER_PATH)
+
+    # Go to appointment page
+    driver.implicitly_wait(3)
+    driver.get(WEBLINK)
+
+    driver = enter_search_parameters(
+        driver=driver,
+        category_choice_text=category_choice_text,
+        service_choice_text=service_choice_text,
+        office_choice_text=office_choice_text
+    )
+    # For now, this returns a hardcoded result:
+    result_dict = scrape_through_search_results(driver)
+
+    driver.quit()
+
+    return result_dict
 
 
 def enter_search_parameters(driver: WebDriver,
@@ -59,3 +95,14 @@ def enter_search_parameters(driver: WebDriver,
     time.sleep(3)  # Initial wait until everything loads
 
     return driver
+
+
+def scrape_through_search_results(driver: WebDriver) -> Optional[Dict[str, str]]:
+    """
+    Loop through each week displayed, through each day of the week, to find possible available times.
+    If available appointment time is displayed, then return a dict with the available time and date.
+    Otherwise, return None.
+    """
+    # TODO: implement scraping through the results
+    result_dict = {'available_time': "8:45", 'date': "26.02"}
+    return result_dict
--- application/tests/test_scraping.py
+++ application/tests/test_scraping.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from application.constants import DRIVER_PATH
 from application.constants import WEBLINK
 from application.scraping import enter_search_parameters
+from application.scraping import scrape_booking_site
 
 
 def test_enter_search_parameters():
@@ -27,3 +28,13 @@ def test_enter_search_parameters():
 
     # Assert that we get "wk" in the resulting page source.
     assert "wk" in driver.page_source
+
+
+def test_scrape_booking_site():
+    """
+    Test for running the main scraping method.
+    """
+    result_dict = scrape_booking_site(category_choice_text="Citizenship",
+                                      service_choice_text="Citizenship application",
+                                      office_choice_text="Helsinki")
+    assert 'available_time' in result_dict.keys()
