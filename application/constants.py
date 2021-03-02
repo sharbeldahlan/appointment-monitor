d3b7aec038ea7ba8271bedf9212f445eaaaa19bb
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2021-03-03 01:27:57 +0200

    Add category, service, and office choice constants
    - This
        1. improves search input and
        2. makes scraping less error-prone.
    
    Since all of those inputs are drop-down lists, adding them as constants
    is a step forward towards creating choice input fields which would be
    used by models and forms in the future.

--- application/constants.py
+++ application/constants.py
@@ -24,6 +24,96 @@ NEXT_WEEK_BUTTON_CLASS_NAME = 'glyphicon-step-forward'
 # Language choice
 ENGLISH_LANGUAGE_CHOICE_TEXT = 'English'
 
+# Migri booking page has Categories and Services:
+# Example: Category --> "Citizenship"; Service --> "Work", "Permanent Residence Permit", etc.
+# Note: only "Citizenship" and "Residence Permit" categories are supported for now
+
+# Service choices - the machine readable ones used for the choices and the mappings
+CITIZENSHIP_APPLICATION = 'citizenship_application'
+CITIZENSHIP_DECLARATION = 'citizenship_declaration'
+CITIZENSHIP_RELEASE = 'citizenship_release'
+CITIZENSHIP_MAARIANHAMINA = 'citizenship_maarianhamina'
+RP_WORK = 'rp_work'
+RP_FAMILY = 'rp_family'
+RP_STUDIES = 'rp_studies'
+RP_OTHER_GROUNDS = 'rp_other_grounds'
+RP_PERMANENT_RESIDENCE = 'rp_permanent'
+RP_RENEWAL = 'rp_renewal'
+
+# Category choices
+CITIZENSHIP_CATEGORY = 'Citizenship'
+RESIDENCE_PERMIT_CATEGORY = 'Residence'
+
+# Citizenship category service texts, as appearing on the booking website:
+# This will be the text with which the search for the elements will happen in scraping.py's find_element.
+CITIZENSHIP_SERVICE_APPLICATION = 'Citizenship application'
+CITIZENSHIP_SERVICE_DECLARATION = 'Citizenship declaration'
+CITIZENSHIP_SERVICE_RELEASE = 'Release'
+CITIZENSHIP_SERVICE_MAARIANHAMINA_MATTERS = 'Ahvenanmaa/Maarianhamina: Citizenship matters'
+
+# Residence permit category service texts, as appearing on the booking website:
+# This will be the text with which the search for the elements will happen in scraping.py's find_element.
+RESIDENCE_PERMIT_SERVICE_WORK = 'Work'
+RESIDENCE_PERMIT_SERVICE_FAMILY = 'Family'
+RESIDENCE_PERMIT_SERVICE_STUDIES = 'Studies'
+RESIDENCE_PERMIT_SERVICE_OTHER_GROUNDS = 'Other grounds'
+RESIDENCE_PERMIT_SERVICE_PERMANENT = 'Permanent'
+RESIDENCE_PERMIT_SERVICE_RENEWAL = 'Renewal'
+
+# Mapping between choice of the service and the Category field
+# format: {<Service choice>: <Text of corresponding category as it appears on the webpage>}
+CATEGORY_TEXT_MAPPING = {
+    CITIZENSHIP_APPLICATION: CITIZENSHIP_CATEGORY,
+    CITIZENSHIP_DECLARATION: CITIZENSHIP_CATEGORY,
+    CITIZENSHIP_RELEASE: CITIZENSHIP_CATEGORY,
+    CITIZENSHIP_MAARIANHAMINA: CITIZENSHIP_CATEGORY,
+    RP_WORK: RESIDENCE_PERMIT_CATEGORY,
+    RP_FAMILY: RESIDENCE_PERMIT_CATEGORY,
+    RP_STUDIES: RESIDENCE_PERMIT_CATEGORY,
+    RP_OTHER_GROUNDS: RESIDENCE_PERMIT_CATEGORY,
+    RP_PERMANENT_RESIDENCE: RESIDENCE_PERMIT_CATEGORY,
+    RP_RENEWAL: RESIDENCE_PERMIT_CATEGORY,
+}
+
+# Mapping between choice of the service and the Service field
+# format: {<Service choice>: <Text of corresponding service as it appears on the webpage>}
+SERVICE_TEXT_MAPPING = {
+    CITIZENSHIP_APPLICATION: CITIZENSHIP_SERVICE_APPLICATION,
+    CITIZENSHIP_DECLARATION: CITIZENSHIP_SERVICE_DECLARATION,
+    CITIZENSHIP_RELEASE: CITIZENSHIP_SERVICE_RELEASE,
+    CITIZENSHIP_MAARIANHAMINA: CITIZENSHIP_SERVICE_MAARIANHAMINA_MATTERS,
+    RP_WORK: RESIDENCE_PERMIT_SERVICE_WORK,
+    RP_FAMILY: RESIDENCE_PERMIT_SERVICE_FAMILY,
+    RP_STUDIES: RESIDENCE_PERMIT_SERVICE_STUDIES,
+    RP_OTHER_GROUNDS: RESIDENCE_PERMIT_SERVICE_OTHER_GROUNDS,
+    RP_PERMANENT_RESIDENCE: RESIDENCE_PERMIT_SERVICE_PERMANENT,
+    RP_RENEWAL: RESIDENCE_PERMIT_SERVICE_RENEWAL,
+}
+
+# Office choices:
+HELSINKI_KAENKUJA = 'helsinki_kaenkuja'
+HELSINKI_MALMI = 'helsinki_malmi'
+KUOPIO = 'kuopio'
+LAHTI = 'lahti'
+LAPPEENRANTA = 'lappeenranta'
+OULU = 'oulu'
+ROVANIEMI = 'rovaniemi'
+TAMPERE = 'tampere'
+TURKU = 'turku'
+VAASA = 'vaasa'
+
+OFFICE_CHOICES = (
+    (HELSINKI_KAENKUJA, 'Helsinki : K'),
+    (HELSINKI_MALMI, 'Helsinki : M'),
+    (KUOPIO, 'Kuopio'),
+    (LAHTI, 'Lahti'),
+    (LAPPEENRANTA, 'Lappeenranta'),
+    (OULU, 'Oulu'),
+    (ROVANIEMI, 'Rovaniemi'),
+    (TAMPERE, 'Tampere'),
+    (TURKU, 'Turku'),
+    (VAASA, 'Vaasa'),
+)
 
 # Usually, the the results of the next 14 weeks are displayed,
 # but we loop 16 times each search just in case more weeks are added.
