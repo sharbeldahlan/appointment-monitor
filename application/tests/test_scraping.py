ed173507b04247a24ed2aca4f54f9a2c14a70719
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2021-03-02 23:17:34 +0200

    Add skeleton for simple test in test_scraping.py

---
+++ application/tests/test_scraping.py
@@ -0,0 +1,9 @@
+def test_enter_search_parameters():
+    """
+    Test that, after entering example search parameters and clicking search,
+    the page shows the available weeks ("wk1", "wk2", "wk3", ...)
+    """
+    # Create a WebDriver instance
+    # Enter some search parameters such as "Citizenship application" and "Helsinki"
+    # Assert that we get "wk" in the resulting page source.
+    assert True
