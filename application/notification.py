cfcb50b8dbdf1b64eed916984b16f94b50599a70
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2021-03-03 11:03:58 +0200

    Add test for sending email correctly
    Add base implementation of send_email()

--- application/notification.py
+++ application/notification.py
@@ -1,7 +1,28 @@
 import smtplib
+from typing import Dict
 
 from application.constants import FROM_EMAIL
 from application.constants import FROM_PASSWORD
+
+
+def send_email(available_appointment: Dict[str, str], to_email: str) -> None:
+    """
+    Main notification function, to send an email to the recipient
+    with the appointment details.
+    """
+    # Extract the results
+    date = available_appointment['date']
+    available_time = available_appointment['available_time']
+
+    # Setup SMTP server and login
+    email_server = setup_smtp_server()
+
+    # Create email
+    # TODO message = ...
+
+    # Send email and quit server
+    # email_server.sendmail(FROM_EMAIL, to_email, message)
+    email_server.quit()
 
 
 def setup_smtp_server() -> smtplib.SMTP:
--- application/tests/test_notification.py
+++ application/tests/test_notification.py
@@ -1,4 +1,30 @@
+from unittest.mock import call
+from unittest.mock import patch
+
+from application.constants import BOOKING_SITE_URL
+from application.constants import FROM_EMAIL
+from application.notification import send_email
 from application.notification import setup_smtp_server
+
+
+@patch('application.notification.setup_smtp_server')
+def test_send_email(mock_email_server):
+    """ Test send_mail() with the SMTP server mocked. """
+    available_appointment = {'available_time': '8:45', 'date': '26.02'}
+    send_email(available_appointment=available_appointment, to_email="test@example.com")
+
+    expected_message = f'Subject: Appointment Spotted!\n\n' \
+                       f'Jee!\n\n' \
+                       f'I spotted an appointment available on 26.02 at 8:45.\n\n' \
+                       f'Book it now on {BOOKING_SITE_URL}.\n\n' \
+                       f'Cheers,\n' \
+                       f'Moni Tore'
+
+    # We expect that there is one call to SMTP server's sendmail() with the following parameters
+    expected_call = call().sendmail(FROM_EMAIL, 'test@example.com', expected_message)
+
+    assert mock_email_server.call_count == 1
+    assert expected_call in mock_email_server.mock_calls
 
 
 def test_setup_smtp_server():
