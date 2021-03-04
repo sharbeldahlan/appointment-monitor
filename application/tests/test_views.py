6ba8c13ebb93b5e571394e56bfb1c5e283c5fdb8
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2021-03-04 21:23:55 +0200

    Add test for posting to the view with valid data
    - Add pytest fixture for posting to the view with valid data

--- application/tests/test_views.py
+++ application/tests/test_views.py
@@ -1,10 +1,23 @@
+import pytest
 from django.test import RequestFactory
 from django.urls import reverse
 
 from application.views import MonitoringRequestCreate
 
 
-# TODO: Add fixture for post response of MonitoringRequestCreate view, after posting with some data
+@pytest.fixture()
+def valid_data_post_response():
+    """ Fixture for response of monitoring_request_view when posted with valid data """
+    factory = RequestFactory()
+    url = reverse('monitoring_request_view')
+    data = {
+        'service_choice': 'citizenship_application',
+        'office_choice': 'Helsinki Käenkuja',
+        'to_email': 'test@example.com'
+    }
+    request = factory.post(path=url, data=data)
+    response = MonitoringRequestCreate.as_view()(request)
+    yield response
 
 
 def test_monitoring_request_view__get__returns_200():
@@ -16,7 +29,8 @@ def test_monitoring_request_view__get__returns_200():
     assert response.status_code == 200
 
 
-# TODO: Test posting to the view with valid data
+def test_monitoring_request_view__post__valid_data__response_as_expected(valid_data_post_response):
+    assert valid_data_post_response.status_code == 200
 
 # TODO: Test posting to the view with invalid data
 
