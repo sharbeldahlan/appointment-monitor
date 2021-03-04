719cc3025a90a8f585e13b627f9cb242210e049e
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2021-03-05 00:37:46 +0200

    Add sanity unit test for scraping logic

--- application/tests/test_scraping.py
+++ application/tests/test_scraping.py
@@ -1,3 +1,6 @@
+from unittest.mock import call
+from unittest.mock import patch
+
 import pytest
 from selenium import webdriver
 
@@ -19,6 +22,30 @@ def driver_in_booking_site():
     yield driver
     # Teardown: we need to quit the driver at the end of the test.
     driver.quit()
+
+
+@patch('application.scraping.enter_search_parameters')
+@patch('application.scraping.scrape_through_search_results')
+@patch('application.scraping.webdriver')
+def test_scrape_booking_site__calls_sub_methods_with_correct_parameters(mock_driver_before_search,
+                                                                        mock_scrape_through_search_results,
+                                                                        mock_enter_search_parameters):
+    """
+    Test scrape_booking_site runs sanely by calling the sub methods:
+        - enter_search_parameters()
+        - scrape_through_search_results()
+    with the correct parameters.
+    """
+    scrape_booking_site(service_choice='foo', office_choice='bar')
+
+    # Assert that the Chrome driver with its state *before* the search is passed to enter_search_parameters()
+    assert mock_enter_search_parameters.call_args == call(driver=mock_driver_before_search.Chrome(),
+                                                          service_choice='foo',
+                                                          office_choice='bar')
+
+    mock_driver_after_search = mock_enter_search_parameters.return_value
+    # Assert that the Chrome driver with its state *after* the search is passed to scrape_through_search_results()
+    assert mock_scrape_through_search_results.call_args == call(driver=mock_driver_after_search)
 
 
 @pytest.mark.parametrize(
@@ -68,7 +95,7 @@ def test_scrape_booking_site(service_choice, office_choice):
 
 
 def test_get_xpath_for_possible_available_time_element():
-    """ Basic test to check that the get xpath for the possible available time works as expected"""
+    """ Basic test to check that the get xpath for the possible available time works as expected """
     expected_xpath = '/html/body/div[4]/div[2]/div/div[5]/div/div[3]/div/div[2]/div[3]/div[1]/a'
     # This is what we are looking for --------------------------------------------> ^
     assert get_xpath_for_possible_available_time_element(3) == expected_xpath
