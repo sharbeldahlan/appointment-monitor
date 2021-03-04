44b42756532ec74577ccc86d61e64126135bdd8a
Sharbel Dahlan <sharbel.dahlan@gmail.com>
2021-03-05 00:21:43 +0200

    Add success message to view and template

--- application/views.py
+++ application/views.py
@@ -1,10 +1,11 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from application.models import MonitoringRequest
 
 
-class MonitoringRequestCreate(CreateView):
+class MonitoringRequestCreate(SuccessMessageMixin, CreateView):
     """
     View to create MonitoringRequest object.
     Upon success, the view redirects to itself.
@@ -12,3 +13,4 @@ class MonitoringRequestCreate(CreateView):
     model = MonitoringRequest
     fields = ['service_choice', 'office_choice', 'to_email']
     success_url = reverse_lazy('monitoring_request_view')
+    success_message = "Appointment monitoring request taken. We're on it! 🦾"
